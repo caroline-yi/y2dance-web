6b84554d8cf7dc72022b112ec770291a9f1ad014
Nicholas Matison <nmatison@gmail.com>
2021-12-27 18:35:37 -0500

    remove weback

--- src/About.js
+++ src/About.js
@@ -1,5 +1,41 @@
 const About = (props) => (
-    <h1> This is the about page </h1>
+    <body>
+        <section class="about">
+            <header>
+                <h1>About y2dance</h1>
+                <p>
+                    "Insert paragraph here about the website. Insert paragraph here about the website. Insert paragraph here about the website.
+                Insert paragraph here about the website. Insert paragraph here about the website. Insert paragraph here about the website.
+                Insert paragraph here about the website. Insert paragraph here about the website. Insert paragraph here about the website.
+                Insert paragraph here about the website. Insert paragraph here about the website. Insert paragraph here about the website.
+                Insert paragraph here about the website. Insert paragraph here about the website. Insert paragraph here about the website.
+                Insert paragraph here about the website. Insert paragraph here about the website. Insert paragraph here about the website.
+                Insert paragraph here about the website. Insert paragraph here about the website. Insert paragraph here about the website.
+                Insert paragraph here about the website. Insert paragraph here about the website. Insert paragraph here about the website."
+                </p>
+            </header>
+            <section class="meet-instructors">
+                <h1>Meet our Instructors</h1>
+                <section class="about-lingyun">
+                    <section>
+                        <h2>Lingyun Yi</h2>
+                        <picture>
+                            <iframe src="https://giphy.com/embed/IB9foBA4PVkKA" width="480" height="474" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+                        </picture>
+                    </section>
+                    <span class="lingyun-bio">
+                        <p class="lingyun-paragraph">
+                            "Lingyun is amazing! Lingyun is amazing! Lingyun is amazing! Lingyun is amazing! Lingyun is amazing! Lingyun is amazing!
+                            Lingyun is amazing! Lingyun is amazing! Lingyun is amazing! Lingyun is amazing! Lingyun is amazing! Lingyun is amazing!
+                            Lingyun is amazing! Lingyun is amazing! Lingyun is amazing! Lingyun is amazing! Lingyun is amazing! Lingyun is amazing!
+                            Lingyun is amazing! Lingyun is amazing! Lingyun is amazing! Lingyun is amazing! Lingyun is amazing! Lingyun is amazing!
+                            Lingyun is amazing! Lingyun is amazing! Lingyun is amazing! Lingyun is amazing! Lingyun is amazing! Lingyun is amazing!"
+                        </p>
+                    </span>
+                </section>
+            </section>
+        </section>
+    </body>
 )
 
 export default About
