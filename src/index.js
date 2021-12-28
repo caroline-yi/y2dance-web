ca009eea990d38e0f7e0b6b900c10315b538a6d2
Lingyun Yi <lingyunyi@Lingyuns-MacBook-Air.local>
2021-12-28 13:44:48 -0800

    Fix issues caused by merge conflict.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
-import App from './App';
-import About from './About';
+
+import Home from './home/home';
+import About from './about/about';
 import Class from './Class';
 import reportWebVitals from './reportWebVitals';
 import { Link } from "react-router-dom";
