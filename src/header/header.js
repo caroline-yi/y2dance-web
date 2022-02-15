aa4e88202b1e53ba4a8d351c332487cda292bf92
Nicholas Matison <nmatison@gmail.com>
2022-02-15 11:45:20 -0500

    Made css more responsive

--- src/header/header.js
+++ src/header/header.js
@@ -1,7 +1,10 @@
 import Navigation from "./navigation/navigation"
 
 const Header = () => (
-    <header>
+    <header className='header'>
+        <picture className="y2dance-logo">
+            <iframe title='peanut-butter-jelly-time' src="https://giphy.com/embed/IB9foBA4PVkKA" width="75" height="75" frameBorder="0"></iframe>
+        </picture>
         <Navigation />
     </header>
 )
--- src/header/navigation/navigation.js
+++ src/header/navigation/navigation.js
@@ -1,18 +1,16 @@
 import { Link } from "react-router-dom";
 
 const Navigation = () => (
-    <nav className="navigation">
-        <ul className='navigation-list'>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/class">Classes</Link>
-            </li>
-            <li>
-                <Link to="/about">About</Link>
-            </li>
-        </ul>
+    <nav className="navigation"> 
+        <li>
+            <Link to="/">Home</Link>
+        </li>
+        <li>
+            <Link to="/class">Classes</Link>
+        </li>
+        <li>
+            <Link to="/about">About</Link>
+        </li>
     </nav>
 )
 
